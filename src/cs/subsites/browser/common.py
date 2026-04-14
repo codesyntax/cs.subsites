b15d323aadd2a8bf74bf7cea2d2c4633ea64085f
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-04-14 10:51:34 +0200

    update implementation

--- src/cs/subsites/browser/common.py
+++ src/cs/subsites/browser/common.py
@@ -7,27 +7,16 @@ from Acquisition import aq_parent, aq_inner
 class LogoViewlet(LogoViewletPlone):
 
     def update(self):
-        super(LogoViewlet, self).update()
-        portal = self.portal_state.portal()
-        bprops = portal.restrictedTraverse('base_properties', None)
-        if bprops is not None:
-            logoName = bprops.logoName
-        else:
-            logoName = 'logo.png'
-        context = aq_inner(self.context)
+        super().update()
+        context = self.context
         while not INavigationRoot.providedBy(context):
             context = aq_parent(context)
 
+        # Override the logo if we are in a subsite
         if ISubSite.providedBy(context):
-            logoTitle = context.Title()
-            if context.restrictedTraverse('@@images').scale('image'):
-                self.img_src = context.restrictedTraverse('@@images').scale('image').url
+            if context.restrictedTraverse("@@images").scale("image"):
+                self.img_src = context.restrictedTraverse("@@images").scale("image").url
                 self.navigation_root_title = context.Title()
             else:
-                logoTitle = self.portal_state.portal_title()
+                # if there is no logo set in the subsite, override just the title
                 self.navigation_root_title = context.Title()
-
-        else:
-            logoTitle = self.portal_state.portal_title()
-            self.logo_tag = portal.restrictedTraverse(logoName).tag(title=logoTitle, alt=logoTitle)
-            self.navigation_root_title = self.portal_state.navigation_root_title()
